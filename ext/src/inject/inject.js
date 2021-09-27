0b0270ff12c4be02383a4a3bcfec13b952f02ff6
Reza Aghajani <rezaaghajani@protonmail.com>
2021-09-27 17:41:20 +0330

    Release Version 1.0

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -2,14 +2,16 @@
     let rate = 1
     let video = document.getElementById("my_video_1_html5_api");
     video.playbackRate = rate
+    const footer = document.querySelector("footer")
+    
+    const about = document.createElement('span')
+    const aboutTxt = document.createElement('p')
+    aboutTxt.textContent = "افزونه تغییر سرعت ویدیو فعال است, ساخته شده با ❤️"
+    about.appendChild(aboutTxt)
+    footer.insertBefore(about,footer.childNodes[0])
 
 
     function updating() {
-         let speed = document.getElementById('nit-speed') 
-         console.log(document.body.contains(speed))
-         if (document.body.contains(speed)) {
-            document.getElementById('nit-speed').remove()
-         }
         const text = document.createElement("p")
         text.setAttribute('id','nit-speed')
         let node = document.createTextNode("سرعت حال حاضر ویدیوی شما: " + rate);
@@ -18,22 +20,25 @@
         document.getElementById("content").insertBefore(text, content.childNodes[0])
     }
 
+
+
     updating()
+
+    let rateNum = document.getElementById('nit-speed')
 
     document.addEventListener('keydown', (e) => {
         if (e.key == ".") {
             rate = rate + 0.5
             video.playbackRate = rate
-            console.log("miao")
-            updating()
+            rateNum.textContent = "سرعت حال حاضر ویدیوی شما: " + rate
         } else if (e.key == ",") {
             rate = rate - 0.5
             video.playbackRate = rate
-            updating()
+            rateNum.textContent = "سرعت حال حاضر ویدیوی شما: " + rate
         } else if (e.key == "/") {
             rate = 1
             video.playbackRate = rate
-            updating()
+            rateNum.textContent = "سرعت حال حاضر ویدیوی شما: " + rate
         }
     });
 
